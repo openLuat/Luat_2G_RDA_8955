d4eb9b9664dec7ded5c05de09a1de5ac509aeede
zhutianhua-qq <278610562@qq.com>
2017-10-29 17:16:11 +0800

    script (1.0.9): 修改记录：     一、lib脚本修改         lib脚本版本号：1.0.9         (1)audio：修正“tts播放时，请求新的tts播放，新的tts播放异常”的问题         (2)gps：修正“不加载agps模块，gps无法正常工作”的问题         (3)sys：修正1.0.7和1.0.8 lib脚本中出现的“远程升级的脚本运行过程中存在语法错误时，无法自动回退到烧写的原始脚本”的问题         (4)支持“单个socket连接出错时，不再直接调用link.shut关闭所有socket连接，先仅关闭当前出错的连接，然后自动重连，只有多次重连连续失败，才去调用link.shut”     二、demo脚本修改         (1)audio：演示“tts播放时，请求新的tts播放”的功能         (2)i2c：演示“i2c.send和i2c.recv”接口的使用         (3)socket、socket_ssl：支持“单个socket连接出错时，不再直接调用link.shut关闭所有socket连接，先仅关闭当前出错的连接，然后自动重连，只有多次重连连续失败，才去调用link.shut”

--- script/demo/http/test.lua
+++ script/demo/http/test.lua
@@ -2,13 +2,7 @@ module(...,package.seeall)
 require"misc"
 require"http"
 require"common"
---[[
-功能介绍：http短连接，首先需要提供ADDR和PORT，该数据就是客户端需要连接的客户端
-1.需要调用函数，来设置url，添加头部，添加实体，这里注意添加首部Host时，与前面的ADDR和PORT一致,利用的是socket的长连接
-2.调用request函数，该函数是发送报文所必需需要调用的
-3.rcvcb函数是接收回调函数，会返回结果，状态码，首部（一个表），实体，该函数是自定义函数，客户可以根据自己的需求自己定义
-4.接收数据后，如果五秒内没有再处理，会重启，会重新连接
-]]
+
 local ssub,schar,smatch,sbyte,slen = string.sub,string.char,string.match,string.byte,string.len
 --测试时请先写出IP地址和端口，后面所写的首部要与这里的host一致，下面的值都是默认的值
 local ADDR,PORT ="www.linuxhub.org",80
@@ -29,23 +23,31 @@ end
 
 --[[
 函数名：rcvcb
-功能：接收回调函数，用户自定义对接收参数进行操作
-参数：result：0：表示 接收实体长度与实际相同，正确输出 1：表示没有实体	2：表示实体超出实际实体，错误，不输出实体内容	3：接收超时	4:表示服务器进行的是分块传输模式
-返回值：
+功能  ：接收回调函数
+参数  ：result：数据接收结果(此参数为0时，后面的几个参数才有意义)
+				0:成功
+				2:表示实体超出实际实体，错误，不输出实体内容
+				3:接收超时
+		statuscode：http应答的状态码，string类型或者nil
+		head：http应答的头部数据，table类型或者nil
+		body：http应答的实体数据，string类型或者nil
+返回值：无
 ]]
 local function rcvcb(result,statuscode,head,body)
-	print("resultrcvcb: ",result)  
-	print("statuscodercvcb: ",statuscode)
-	if	head==nil	then	print("headrcvcb:	nil")
-	else
-		print("headrcvcb:")
-		--遍历打印出所有头部，键为首部名字，键所对应的值为首部的字段值
-		for k,v in pairs(head) do		
-			print(k..": "..v)
+	print("rcvcb",result,statuscode,head,slen(body))
+	
+	if result==0 then
+		if head then
+			print("rcvcb head:")
+			--遍历打印出所有头部，键为首部名字，键所对应的值为首部的字段值
+			for k,v in pairs(head) do		
+				print(k..": "..v)
+			end
 		end
+		print("rcvcb body:")
+		print(body)
 	end
-	print("bodyrcvcb:")
-	print(body)
+	
 	httpclient:disconnect(discb)
 end
 
@@ -57,13 +59,8 @@ end
 返回值：
 ]]
 local function connectedcb()
-	local cmdtyp="GET"
-	local url="/"
-	--这里注意，head是table类型，键值必须是XXXX: XXXX格式
-	local head={"Host: 112.29.250.194"}
-	local body=""
 	--调用此函数才会发送报文,request(cmdtyp,url,head,body,rcvcb),回调函数rcvcb(result,statuscode,head,body)
-    httpclient:request(cmdtyp,url,head,body,rcvcb)
+    httpclient:request("GET","/",{},"",rcvcb)
 end 
 
 --[[
@@ -108,7 +105,8 @@ end
 ]]
 function http_run()
 	--因为http协议必须基于“TCP”协议，所以不必传入PROT参数
-	httpclient=http.create(ADDR,PORT)	
+	httpclient=http.create(ADDR,PORT)
+	--httpclient:setconnectionmode(true)
 	--建立http连接
 	connect()	
 end
--- script/lib/agpsupgpd.lua
+++ script/lib/agpsupgpd.lua
@@ -82,15 +82,14 @@ local function reconn()
 	--一个连接周期内的重连
 	if reconncnt < RECONN_MAX_CNT then		
 		reconncnt = reconncnt+1
-		link.shut()
-		connect()
+		socket.disconnect(SCK_IDX)
 	--一个连接周期的重连都失败
 	else
 		reconncnt,reconncyclecnt = 0,reconncyclecnt+1
 		if reconncyclecnt >= RECONN_CYCLE_MAX_CNT then
 			sys.restart("connect fail")
 		end
-		sys.timer_start(reconn,RECONN_CYCLE_PERIOD*1000)
+		link.shut()
 	end
 end
 
@@ -124,24 +123,22 @@ function ntfy(idx,evt,result,item)
 	elseif evt == "SEND" then
 		--发送失败，RECONN_PERIOD秒后重连后台，不要调用reconn，此时socket状态仍然是CONNECTED，会导致一直连不上服务器
 		if not result then sys.timer_start(reconn,RECONN_PERIOD*1000) end
-		if not result then link.shut() end
+		if not result then socket.disconnect(idx) end
 	--连接被动断开
 	elseif evt == "STATE" and result == "CLOSED" then
 		linksta = false
-		--reconn()
 	--连接主动断开（调用link.shut后的异步事件）
 	elseif evt == "STATE" and result == "SHUTED" then
 		linksta = false
+		connect()
 	--连接主动断开（调用socket.disconnect后的异步事件）
 	elseif evt == "DISCONNECT" then
 		linksta = false
-		--reconn()		
+		connect()
 	end
 	--其他错误处理，断开数据链路，重新连接
 	if smatch((type(result)=="string") and result or "","ERROR") then
-		--RECONN_PERIOD秒后重连，不要调用reconn，此时socket状态仍然是CONNECTED，会导致一直连不上服务器
-		--sys.timer_start(reconn,RECONN_PERIOD*1000)
-		link.shut()
+		socket.disconnect(idx)
 	end
 end
 
@@ -365,8 +362,6 @@ function checkup()
 	end
 end
 
---为GPS提供32K时钟
-rtos.sys32k_clk_out(1)
 --注册GPS消息处理函数
 sys.regapp(proc,"AGPS_WRDATE_SUC")
 sys.regapp(gpsstateind,gps.GPS_STATE_IND)
