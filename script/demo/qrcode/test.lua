81bdafa550df065fa7411c10b9855e834b939814
lisiqi <alienwalker@sina.com>
2017-11-11 19:13:33 +0800

    增加二维码 LCD屏显示接口，ST7735上测试成功

--- script/demo/qrcode/test.lua
+++ script/demo/qrcode/test.lua
@@ -1,12 +1,8 @@
---[[
-模块名称：电话本测试
-模块功能：测试电话本读写
-模块最后修改时间：2017.05.23
-]]
-
 module(...,package.seeall)
-require"qrencode"
+require"qrcode"
+require"string"
 
+WIDTH,HEIGHT,BPP = disp.getlcdinfo()
 --[[
 函数名：print
 功能  ：打印接口，此文件中的所有打印都会加上test前缀
@@ -14,33 +10,16 @@ require"qrencode"
 返回值：无
 ]]
 local function print(...)
-	_G.print("ljd test",...)
+	_G.print("test",...)
 end
 
-local function test_print()
-	print("enter test_main.....")
+function test_once()
+	local buf,w,h,isfix = qrcode.create("www.baidu.com1234567890",WIDTH,HEIGHT)
+	print(buf,#buf, w,h,isfix)
+	disp.setbkcolor(0xffff)
+	disp.clear()
+	qrcode.display(buf,w,h,(WIDTH - w)/2,(HEIGHT - h)/2,0)
+	disp.update()
 end
 
-local function test_main_obj()
-    local qr_object = qrencode.create("/ldata/test1.jpg")
-
-    print("test_main_obj create",qr_object)
-
-    local wd,ht = qr_object:info()
-
-    print("test_main_obj info",wd,ht)
-
-    local buf = qr_object:buffer()
-
-    print("test_main_obj buffer",buf)
-
-   qr_object:destory()
-
-    print("test_main_obj destory")
-end
-
-
---循环定时器只是为了判断PB功能模块是否ready
-sys.timer_loop_start(test_print,2000)
-
-sys.timer_loop_start(test_main_obj,5000)
+sys.timer_start(test_once, 5000)
