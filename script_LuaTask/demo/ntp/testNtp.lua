61198fc888ea87ab3a7f41e98a3208f585842898
zhutianhua <zhutianhua@airm2m.com>
2018-06-12 17:25:26 +0800

    发布script_LuaTask(2.0.4): 修改记录：     一、lib脚本修改 	lib脚本版本号：2.0.4 	(1)gps.lua、agps.lua：修正“gps关闭后，再打开gps，gps定位慢”的问题 		【问题重要性】：重要 		【问题影响模块】：Air800 		【问题出现概率】：必现 		【问题出现原因】：再次打开GPS时，没有写入位置和时间数据到GPS芯片，就无法加速GPS定位，相当于GPS芯片的秒定位功能没有生效 		【问题解决方式】：每次开启GPS，都写入上次关闭前的位置（基站位置或者GPS位置）和当前的时间数据 	(2)link.lua：移动卡默认APN由CMNET修改为CMIOT 		【新功能重要性】：重要 		【新功能影响模块】：Air202、Air800、Air801、Air201、Air208 		【新功能说明】：有用户反映，移动物联网卡，使用原来的CMNET，可能无法正常上网，使用CMIOT正常，所以修改为CMIOT 	(3)ntp.lua：NTP服务器由原来的顺序遍历修改为随机遍历 		【新功能重要性】：重要 		【新功能影响模块】：Air202、Air800、Air801、Air201、Air208 		【新功能说明】：无论是顺序遍历还是随机遍历，都要注意： 				ntp功能模块采用多个免费公共的NTP服务器来同步时间，不能保证任何时间任何地点都能百分百同步到正确的时间 				如果用户项目中的业务逻辑严格依赖于时间同步功能，则不要使用使用本功能模块，建议使用自己的应用服务器来同步时间 	(4)patch.lua：新增“协程运行错误时，输出traceback信息”功能 		【新功能重要性】：一般 		【新功能影响模块】：Air202、Air800、Air801、Air201、Air208 		【新功能说明】：旧版本，协程运行出错时，仅输出最后出错的代码位置；新版本出错时，会输出整个调试栈的所有代码位置

--- script_LuaTask/demo/ntp/testNtp.lua
+++ script_LuaTask/demo/ntp/testNtp.lua
@@ -4,16 +4,21 @@
 -- @license MIT
 -- @copyright openLuat
 -- @release 2018.03.28
-module(..., package.seeall)
 
-require "ntp"
-require "misc"
+module(...,package.seeall)
+
+-- 重要提醒！！！！！！
+-- ntp功能模块采用多个免费公共的NTP服务器来同步时间
+-- 并不能保证任何时间任何地点都能百分百同步到正确的时间
+-- 所以，如果用户项目中的业务逻辑严格依赖于时间同步功能
+-- 则不要使用使用本功能模块，建议使用自己的应用服务器来同步时间
+require"ntp"
+require"misc"
 
 local function prinTime()
     local tm = misc.getClock()
-    -- log.info("testNtp.printTime",tm.year,tm.month,tm.day,tm.hour,tm.min,tm.sec)
     log.info("testNtp.printTime", string.format("%04d/%02d/%02d,%02d:%02d:%02d", tm.year, tm.month, tm.day, tm.hour, tm.min, tm.sec))
 end
 
-sys.timerLoopStart(prinTime, 1000)
+sys.timerLoopStart(prinTime,1000)
 ntp.timeSync()
