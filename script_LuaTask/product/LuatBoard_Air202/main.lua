dabde1137705764fd96676fddfd23c89494b9711
hotdll <hotdll@163.com>
2018-07-01 15:51:39 +0800

    增加MP3,增加I2C代码，增加远程更新，完成协议文档，版本号为1.0.3

--- script_LuaTask/product/LuatBoard_Air202/main.lua
+++ script_LuaTask/product/LuatBoard_Air202/main.lua
@@ -1,8 +1,8 @@
 --必须在这个位置定义PROJECT和VERSION变量
 --PROJECT：ascii string类型，可以随便定义，只要不使用,就行
 --VERSION：ascii string类型，如果使用Luat物联云平台固件升级的功能，必须按照"X.X.X"定义，X表示1位数字；否则可随便定义
-PROJECT = "LuatBoard-Air202"
-VERSION = "0.0.1"
+PROJECT = "LUATBOARD-AIR202"
+VERSION = "1.0.3"
 PRODUCT_KEY = "aVyWNJl9CX6RRT9DKusErrrkH87NzvWo"
 -- 日志级别
 require "log"
@@ -27,7 +27,6 @@ wdt.setup(pio.P0_30, pio.P0_31)
 -- 系統指示灯
 require "default"
 require "ntp"
-ntp.timeSync(1, function()log.info("----------------> AutoTimeSync is Done ! <----------------") end)
 require "audio"
 -- 启动系统框架
 sys.init(0, 0)
