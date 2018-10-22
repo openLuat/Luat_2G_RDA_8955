3d3538e07950c7469d6f8e9d919cca51f62245fb
hotdll <hotdll@163.com>
2018-10-22 15:40:56 +0800

    新增uartv3的demo,不用任务直接用回调的方式，支持任务和回调读取

---
+++ script_LuaTask/demo/uartv3/demo.lua
@@ -0,0 +1,35 @@
+--- 模块功能：串口功能测试(TASK版)
+-- @author openLuat
+-- @module uart.testUartTask
+-- @license MIT
+-- @copyright openLuat
+-- @release 2018.10.20
+require "utils"
+require "pm"
+module(..., package.seeall)
+
+
+-------------------------------------------- 配置串口 --------------------------------------------
+-- 串口ID,串口读缓冲区
+local UART_ID, sendQueue = 1, {}
+-- 串口超时，串口准备好后发布的消息
+local uartimeout, recvReady = 25, "UART_RECV_ID"
+--保持系统处于唤醒状态，不会休眠
+pm.wake("mcuart")
+uart.setup(UART_ID, 115200, 8, uart.PAR_NONE, uart.STOP_1)
+uart.on(1, "receive", function(uid)
+    table.insert(sendQueue, uart.read(uid, 1460))
+    sys.timerStopAll(sys.publish)
+    sys.timerStart(sys.publish, uartimeout, recvReady, table.concat(sendQueue))
+end)
+
+-- 向串口发送收到的字符串
+sys.subscribe(recvReady, function(str)
+    log.info("uart read length:", #str, str)
+    -- 串口写缓冲区最大1460
+    for i = 1, #str, 1460 do
+        uart.write(UART_ID, str:sub(i, i + 1460 - 1))
+    end
+    -- 串口的数据可以用消息收取，也可以直接读串口缓区表的数据,读完后清空缓冲区
+    sendQueue = {}
+end)
---
+++ script_LuaTask/demo/uartv3/main.lua
@@ -0,0 +1,65 @@
+--必须在这个位置定义PROJECT和VERSION变量
+--PROJECT：ascii string类型，可以随便定义，只要不使用,就行
+--VERSION：ascii string类型，如果使用Luat物联云平台固件升级的功能，必须按照"X.X.X"定义，X表示1位数字；否则可随便定义
+PROJECT = "UART3"
+VERSION = "2.0.0"
+
+--加载日志功能模块，并且设置日志输出等级
+--如果关闭调用log模块接口输出的日志，等级设置为log.LOG_SILENT即可
+require "log"
+LOG_LEVEL = log.LOGLEVEL_TRACE
+--[[
+如果使用UART输出日志，打开这行注释的代码"--log.openTrace(true,1,115200)"即可，根据自己的需求修改此接口的参数
+如果要彻底关闭脚本中的输出日志（包括调用log模块接口和Lua标准print接口输出的日志），执行log.openTrace(false,第二个参数跟调用openTrace接口打开日志的第二个参数相同)，例如：
+1、没有调用过sys.opntrace配置日志输出端口或者最后一次是调用log.openTrace(true,nil,921600)配置日志输出端口，此时要关闭输出日志，直接调用log.openTrace(false)即可
+2、最后一次是调用log.openTrace(true,1,115200)配置日志输出端口，此时要关闭输出日志，直接调用log.openTrace(false,1)即可
+]]
+--log.openTrace(true,1,115200)
+
+require "sys"
+
+require "net"
+--每1分钟查询一次GSM信号强度
+--每1分钟查询一次基站信息
+net.startQueryAll(60000, 60000)
+
+--加载控制台调试功能模块（此处代码配置的是uart1，波特率115200）
+--此功能模块不是必须的，根据项目需求决定是否加载
+--使用时注意：控制台使用的uart不要和其他功能使用的uart冲突
+--使用说明参考demo/console下的《console功能使用说明.docx》
+--require "console"
+--console.setup(1, 115200)
+
+--加载硬件看门狗功能模块
+--根据自己的硬件配置决定：1、是否加载此功能模块；2、配置Luat模块复位单片机引脚和互相喂狗引脚
+--合宙官方出售的Air201开发板上有硬件看门狗，所以使用官方Air201开发板时，必须加载此功能模块
+--[[
+require "wdt"
+wdt.setup(pio.P0_30, pio.P0_31)
+]]
+
+--加载网络指示灯功能模块
+--根据自己的项目需求和硬件配置决定：1、是否加载此功能模块；2、配置指示灯引脚
+--合宙官方出售的Air800和Air801开发板上的指示灯引脚为pio.P0_28，其他开发板上的指示灯引脚为pio.P1_1
+--require "netLed"
+--netLed.setup(true,pio.P1_1)
+--网络指示灯功能模块中，默认配置了各种工作状态下指示灯的闪烁规律，参考netLed.lua中ledBlinkTime配置的默认值
+--如果默认值满足不了需求，此处调用netLed.updateBlinkTime去配置闪烁时长
+
+--加载错误日志管理功能模块【强烈建议打开此功能】
+--如下2行代码，只是简单的演示如何使用errDump功能，详情参考errDump的api
+require "errDump"
+errDump.request("udp://ota.airm2m.com:9072")
+
+--加载远程升级功能模块【强烈建议打开此功能】
+--如下3行代码，只是简单的演示如何使用update功能，详情参考update的api以及demo/update
+--PRODUCT_KEY = "v32xEAKsGTIEQxtqgwCldp5aPlcnPs3K"
+--require "update"
+--update.request()
+
+--加载串口功能测试模块（串口1，TASK方式实现，串口帧超时分帧）
+require "demo"
+
+--启动系统框架
+sys.init(0, 0)
+sys.run()
