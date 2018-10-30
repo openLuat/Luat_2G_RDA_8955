c58e1ed68ff19b7de977e58b226c2e7cb863d898
zhangtao <zhangtao@openluat.com>
2018-10-30 11:20:46 +0800

    testGps代码没提交成功

--- script_LuaTask/demo/gpsv2/testGps.lua
+++ script_LuaTask/demo/gpsv2/testGps.lua
@@ -4,17 +4,13 @@
 -- @license MIT
 -- @copyright openLuat
 -- @release 2018.10.29
-require "gps"
+require "gpsv2"
 module(..., package.seeall)
-
-
 
 sys.taskInit(function(...)
     gpsv2.open(2, 115200, 2, 5)
     while true do
-        local tLocation = gps.getLocation()
-        local speed = gps.getSpeed()
-        log.infi("testGps isFix:", gpsv2.isFix())
+        log.info("testGps isFix:", gpsv2.isFix())
         log.info("testGps lng,lat:", gpsv2.getIntLocation())
         log.info("testGps message:", gpsv2.getAltitude(), gpsv2.getSpeed(),
             gpsv2.getAzimuth(), gpsv2.getUsedSateCnt(), gpsv2.getViewedSateCnt())
