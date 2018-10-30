1a4d86a8c5c39f9e9116186a6fa492a721947259
zhangtao <zhangtao@openluat.com>
2018-10-30 11:28:30 +0800

    增加gpsv2的demo的key

--- script_LuaTask/demo/gpsv2/main.lua
+++ script_LuaTask/demo/gpsv2/main.lua
@@ -3,6 +3,7 @@
 --VERSION：ascii string类型，如果使用Luat物联云平台固件升级的功能，必须按照"X.X.X"定义，X表示1位数字；否则可随便定义
 PROJECT = "GPSV2"
 VERSION = "2.0.0"
+PRODUCT_KEY = "v32xEAKsGTIEQxtqgwCldp5aPlcnPs3K"
 
 --加载日志功能模块，并且设置日志输出等级
 --如果关闭调用log模块接口输出的日志，等级设置为log.LOG_SILENT即可
